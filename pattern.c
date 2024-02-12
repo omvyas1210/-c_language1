1d5bf9fa7225b55aec835162c17b5cee6958a059
omvyas1210 <122439388+omvyas1210@users.noreply.github.com>
2024-02-12 19:27:00 +0530

    https://github.com/omvyas1210/-c_language1/upload
    https://github.com/omvyas1210/-c_language1/upload

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	printf("-------------\n");
+	
+	printf("|           |\n");
+	
+	printf("R           |\n");
+	
+	printf("N           |\n");
+	
+	printf("|           |\n");
+	
+	printf("---------------");
+	
+	
+}
